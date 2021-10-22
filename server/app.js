b37a9a911f9ab8bf5a3b6166ff2f98b160fca3a9
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-22 15:46:10 -0700

    Generates environment variable needed to access IMDB from my own API key.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
-
 const app = express();
+require('dotenv').config();
 
+
+const api = process.env.API_KEY
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
 module.exports = app;
