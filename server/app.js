d2e33e7186a31ca1fe1b1b93362a2dcee5dbc321
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-22 18:41:14 -0700

    Axios call obtains data from omdb.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,35 @@
-const express = require('express');
-const app = express();
+//iimporting/requiring files
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var axios = require('axios');
 require('dotenv').config();
 
+//creating app server
+var app = express();
+var movies = {};
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-const api = process.env.API_KEY
+
+app.get('/', function(req, res){
+
+  res.status(200).send(movies[req.query.i]);
+
+  if (movies[req.query.i]){
+    return;
+  } else {
+  axios({
+    method: 'get',
+    url: `http://www.omdbapi.com/?i=${req.query.i}&apikey=${process.env.API_KEY}`,
+  })
+    .then(res => {
+      movies[req.query.i] = res.data;
+    });
+  }
+})
+
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
 module.exports = app;
