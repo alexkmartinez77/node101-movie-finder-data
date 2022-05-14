8f62b420c8a8be9535a1caaf9168faffac931eab
Alex Martinez <alexkmartinez77@gmail.com>
2022-05-13 17:21:37 -0700

    Movie Finder Update

--- server/app.js
+++ server/app.js
@@ -1,29 +1,23 @@
-//iimporting/requiring files
 var express = require('express');
 var morgan = require('morgan');
-var bodyParser = require('body-parser');
 var axios = require('axios');
 require('dotenv').config();
 
-//creating app server
+
 var app = express();
-var movies = [];
+var cache = [];
 var movieKeys = {
   i: 'imdbID',
   t: 'Title'
 }
 
-//middleware
 app.use(morgan('dev'));
-app.use(bodyParser.json());
 
-//routes
 app.get('/', function(req, res){
   // Returns false if not found and returns the actual movie object if found
   const findMovie = (key, value) => {
     let found = false;
-    //console.log('movies', movies,'key',key, 'value', value);
-    movies.forEach(movie => {
+    cache.forEach(movie => {
       console.log('movie[key]', movie[key], 'value', value);
       if (movie[key].toLowerCase() == value.toLowerCase()) 
         found = movie;
@@ -37,14 +31,14 @@ app.get('/', function(req, res){
       res.status(200).send(foundMovie);
     } else {
       axios({method: 'get', url: `http://www.omdbapi.com/?${key}=${value}&apikey=${process.env.API_KEY}`,})
-      .then(movie => {movies.push(movie.data); res.status(200).send(movie.data)})
+      .then(movie => {cache.push(movie.data); res.status(200).send(movie.data)})
       .catch((e) => {console.log(e.message); res.send('OMDB Error.')});
     }
   }
 
   // Assign incoming key to variable x
   let x = (Object.keys(req.query)[0]);
-  //Look for movie in movies array and return movie or boolean false
+  //Look for movie in cache array and return movie or boolean false
   let movie = findMovie(movieKeys[x], req.query[x]);
   //If false returned then make axios call and push movie into movie array
   axiosCall(movie, x, req.query[x]);
