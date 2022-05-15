df61e695e045d52afbf2901d55e45d5d524be241
Alex Martinez <alexkmartinez77@gmail.com>
2022-05-14 18:44:12 -0700

    Reformatted code

--- server/app.js
+++ server/app.js
@@ -3,23 +3,22 @@ var morgan = require('morgan');
 var axios = require('axios');
 require('dotenv').config();
 
-
 var app = express();
 var cache = [];
 
 app.use(morgan('dev'));
+app.set('json spaces', 2);
 
 app.get('/', function(req, res){
 
-  let foundMovie = cache.find(movie => movie[req.url[2] == 'i' ? 'imdbID' : 'Title'].toLowerCase() == Object.values(req.query).shift())
-  if(foundMovie != undefined){
+  let foundMovie = cache.find(movie => movie[Object.keys(req.query).shift() == 'i' ? 'imdbID' : 'Title'].toLowerCase() == Object.values(req.query).shift())
+  if(foundMovie){
     res.status(200).send(foundMovie);
   } else {
-    axios({method: 'get', url: `http://www.omdbapi.com${req.url}&apikey=${process.env.API_KEY}`,})
-    .then(movie => {cache.push(movie.data); res.status(200).send(movie.data)})
-    .catch((e) => {console.log(e.message); res.send('Please make a valid OMDB query.')});
+    axios.get(`http://www.omdbapi.com${req.url}&apikey=${process.env.API_KEY}`)
+         .then(movie => {cache.push(movie.data); res.status(200).send(movie.data)})
+         .catch((e) => {console.log(e.message); res.send('Please make a valid OMDB query.')});
   }
-  console.log(cache)
 })
 
 module.exports = app;
